8157859842bcd9b0d9b3a2a9a0a70f3b0dd72a2b
braimohigboibroheem <braimohigboibroheem@gmail.com>
2025-01-21 21:35:24 -0800

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import { useState } from 'react'
 
 
 
-import Navbar from './components/static/navbar/navbar'
-import Hero from './components/pages/hero/hero'
-import About from './components/pages/about/about'
+import Navbar from './components/static/navbar/Navbar'
+import Hero from './components/pages/hero/Hero'
+import About from './components/pages/about/About'
 import Car from './components/pages/car/Car'
 import Value from './components/pages/value/Value'
 import Testimony from './components/pages/testimony/Testimony'
 import Brief from './components/home/Brief/Brief'
-import Qualities from './components/home/qualities/qualities'
+import Qualities from './components/home/qualities/Qualities'
 import Cartwo from './components/home/carTwo/Cartwo'
-import Footer from './components/static/footer/footer'
+import Footer from './components/static/footer/Footer'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -27,8 +28,8 @@ function App() {
 <Testimony />
 <Qualities />
 <Cartwo />
-<Footer />
 
+<Footer />
 
  </div>
   )
